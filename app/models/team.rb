a58766cc64c0ec85c15b8908cc09c4dc9aa3b221
Mario Zigliotto <mariozig@gmail.com>
2014-06-25 08:30:34 -0700

    Show matches a team has played

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,11 @@
 class Team < ActiveRecord::Base
   has_many :players
+  has_many :home_matches, foreign_key: 'home_team_id', class_name: 'Match'
+  has_many :away_matches, foreign_key: 'away_team_id', class_name: 'Match'
 
   default_scope { order(:name) }
+
+  def matches
+    home_matches + away_matches
+  end
 end
