61b77ac7042d78424389731436af1236307ddbe8
Mario Zigliotto <mariozig@gmail.com>
2014-06-24 22:39:57 -0700

    Build out a basic show page for individual matches

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,8 @@ class Match < ActiveRecord::Base
   belongs_to :away_team, foreign_key: 'away_team_id', class_name: 'Team'
 
   default_scope { order(:start_time) }
+
+  def winning_team
+    home_score > away_score ? home_team : away_team
+  end
 end
