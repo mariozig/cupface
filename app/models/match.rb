ace2418c1b306d7bde5988d53438b817c76c2850
Mario Zigliotto <mariozig@gmail.com>
2014-06-24 22:30:54 -0700

    Add matches controller with index and show actions + supporting routes

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ActiveRecord::Base
   belongs_to :home_team, foreign_key: 'home_team_id', class_name: 'Team'
   belongs_to :away_team, foreign_key: 'away_team_id', class_name: 'Team'
+
+  default_scope { order(:start_time) }
 end
