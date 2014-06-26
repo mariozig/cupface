3e4584c66da5adcb5428fe2edbe7eb4d987b2488
Mario Zigliotto <mariozig@gmail.com>
2014-06-26 00:06:48 -0700

    Add paperclip initializer that sets up required keys for AWS S3.

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,6 @@
+Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:s3_credentials] = {
+  bucket: ENV['S3_BUCKET'],
+  access_key_id: ENV['S3_ACCESS_KEY_ID'],
+  secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
+}
